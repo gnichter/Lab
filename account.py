21bc17799ae7ffa90c49f27a3d648be911a7980e
gnichter <gnichter2@huskers.unl.edu>
2022-11-20 21:12:25 -0600

    last commit

--- account.py
+++ account.py
@@ -1,25 +1,33 @@
 class Account:
 
-    def __init__(self, name):
-        self.__account_name = name
-        self.__account_balance = 0
+    def init(self, name):
+        self._account_name: str = f'{name}'
+        self._account_balance: int = 0
+    '''This function initializes the account'''
 
-    def deposit(self, amount):
-        amount = float(input("Enter amount to be deposited: "))
+    def deposit(self, amount: int) -> bool:
         if amount > 0:
-            self.__account_balance += amount
+            self._account_balance += amount
+            return True
         else:
-            pass
+            return False
 
-    def withdraw(self, amount):
-        amount = float(input("Enter amount to be Withdrawn: "))
-        if self.__account_balance >= amount:
-            self.__account_balance -= amount
+    '''This function sees if your deposit is a valid positive number and
+    if it is deposits it into the account'''
+
+    def withdraw(self, amount: int) -> bool:
+        if amount > 0:
+            self._account_balance -= amount
+            return True
         else:
-            pass
+            return False
+    '''This function sees if your withdrawal is a valid positive number
+        and if it is, that amount is subtracted to the account'''
 
-    def get_balance(self):
-        print(self.__account_balance)
+    def get_balance(self) -> str:
+        return f'{self._account_balance}'
+    '''This function returns the amount left in the account'''
 
-    def get_name(self):
-        print(self.__account_name)
+    def get_name(self) -> str:
+        return f'{self._account_name}'
+    '''This function returns the name from the account'''
