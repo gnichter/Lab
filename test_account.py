840b26d67f4d60a6bf297dbfb5d36f42056adec7
gnichter <gnichter2@huskers.unl.edu>
2022-11-20 21:09:10 -0600

    Update test_account.py

--- test_account.py
+++ test_account.py
@@ -1,9 +1,29 @@
-import account
+import pytest
+from account import Account
 
-account = Account()
-print(account.deposit(100))
-print(account._account_balance)
-print(account.deposit(0))
-print(account._account_balance)
-print(account.deposit(-100))
-print(account._account_balance)
+
+def testinit():
+    testName = Account('Jimmy')
+    assert testName.get_name() == 'Jimmy'
+    assert testName.get_balance() == 0
+
+
+def test_deposit():
+    testName = Account('Jimmy')
+    testName.deposit(-70)
+    assert testName.get_balance() == 0
+    testName.deposit(70)
+    assert testName.get_balance() == 70
+    testName.deposit(0)
+    assert testName.get_balance() == 70
+
+
+def test_withdarawl():
+    testName = Account('Jimmy')
+    testName.deposit(90)
+    testName.withdraw(-75)
+    assert testName.get_balance() == 0
+    testName.deposit(75)
+    assert testName.get_balance() == 15
+    testName.deposit(0)
+    assert testName.get_balance() == 15
